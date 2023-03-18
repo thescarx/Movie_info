ac413fabada5b1a9bba091e884c3707921ea75b8
adScarX <aa.djellouli@esi-sba.dz>
2023-03-18 01:58:47 +0100

    f

--- movie_infos/lib/home.dart
+++ movie_infos/lib/home.dart
@@ -1,14 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add a note'),
+        title: Center(child: Text('Home')),
+        actions: [
+          IconButton(
+              onPressed: () {
+                addNote();
+              },
+              icon: Icon(Icons.add))
+        ],
       ),
-      body: Scaffold(),
+      body: Center(
+        child: Text('Hi'),
+      ),
     );
+  }
+
+  Future addNote() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Add note'),
+            content: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Title',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xffE4E9F2), width: 1.5),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                            color: Color(0xffE4E9F2), width: 1.5),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(
+                            color: Color(0xffE4E9F2), width: 1.5),
+                      ),
+                      hintText: "Here goes title",
+                      hintStyle: const TextStyle(
+                        color: Color(0xff8F9BB3),
+                        fontSize: 15,
+                        fontFamily: "Tajawal",
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Description'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: descController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xffE4E9F2), width: 1.5),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                            color: Color(0xffE4E9F2), width: 1.5),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(
+                            color: Color(0xffE4E9F2), width: 1.5),
+                      ),
+                      hintText: "Here goes description",
+                      hintStyle: const TextStyle(
+                        color: Color(0xff8F9BB3),
+                        fontSize: 15,
+                        fontFamily: "Tajawal",
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        });
   }
 }
