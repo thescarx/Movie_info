9405649c4ec9f10f37bca2e40b8e644555a9bd73
adScarX <aa.djellouli@esi-sba.dz>
2023-03-18 01:47:52 +0100

    f

--- movie_infos/lib/home.dart
+++ movie_infos/lib/home.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:movie_infos/body.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Body(),
+      appBar: AppBar(
+        title: Text('Add a note'),
+      ),
+      body: Scaffold(),
     );
   }
 }
