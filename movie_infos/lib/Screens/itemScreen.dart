a4704bd055b07bfe1c393d5ea4eb44801ee8c70d
oussama boughanem <go_boughanem@esi.dz>
2023-03-18 01:59:26 +0100

    Create itemScreen.dart

---
+++ movie_infos/lib/Screens/itemScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemScreen extends StatefulWidget {
+  const ItemScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ItemScreen> createState() => _ItemScreenState();
+}
+
+class _ItemScreenState extends State<ItemScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
